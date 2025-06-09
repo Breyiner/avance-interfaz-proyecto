85c250d3d4fcfd290931faa4d484ce7fd4ddb2ba
Breyiner <breyneracosta7@gmail.com>
2025-06-09 12:23:47 -0500

    avance en el inicio

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-document.querySelector("#login").addEventListener("click", () => {
-  window.location.assign("http://127.0.0.1:5500/inicio.html"); // Reemplaza con la nueva ruta
+let abrir = document.querySelector('#abrirModal');
+let cerrar = document.querySelector('#cerrarModal');
+
+// document.querySelector("#login").addEventListener("click", () => {
+//   window.location.assign("http://127.0.0.1:5500/inicio.html");
+// });
+
+abrir.addEventListener('click', () => {
+  document.querySelector('#modal').showModal();
+});
+
+cerrar.addEventListener('click', () => {
+  document.querySelector('#modal').close();
 });
