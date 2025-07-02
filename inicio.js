6e26b7554103d7b4f5edc1476a6f60065a1ee5c9
Breyiner <breyneracosta7@gmail.com>
2025-07-02 18:45:23 -0500

    creacion de ventana emergente

--- inicio.js
+++ inicio.js
@@ -1,4 +1,8 @@
 let usuario = JSON.parse(localStorage.getItem("usuario"));
+const movimiento = document.querySelector(".movimiento");
+const modal = document.querySelector(".modal");
+let cerrar = document.querySelector('.modal__salir');
+
 
 if (usuario) {
   let nombreHeader = document.querySelector('.profile__nombre');
@@ -7,3 +11,14 @@ if (usuario) {
   nombreHeader.textContent = `${usuario.nombre} ${usuario.apellido}`;
   nombreMenu.textContent = `${usuario.nombre} ${usuario.apellido}`;
 }
+
+
+movimiento.addEventListener("click", (e) => {
+  document.querySelector('.modal').showModal();
+  modal.classList.add("mostrar"); // Mostrar modal
+});
+
+cerrar.addEventListener('click', () => {
+  document.querySelector('.modal').close();
+  modal.classList.remove("mostrar"); // Ocultar modal
+});
