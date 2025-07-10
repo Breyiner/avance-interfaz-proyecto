a61042d5bd5d250ceddf0a10efa242eff309cb30
Breyiner <breyneracosta7@gmail.com>
2025-07-10 18:47:22 -0500

    cambios menores

--- calendario.js
+++ calendario.js
@@ -13,7 +13,31 @@ document.addEventListener('DOMContentLoaded', function () {
       month: 'Mes',
       week: 'Semana',
       day: 'Día'
-    }
+    },
+    selectable: true,
+    select: function(info) {
+      const titulo = prompt('Título del evento:')
+      if (titulo) {
+        calendar.addEvent({
+          title: titulo,
+          start: info.startStr,
+          end: info.endStr,
+          allDay: info.allDay
+        });
+      }
+    },
+    events: [
+      {
+        title: 'Reunión',
+        start: '2025-07-15'
+      }
+    ]
   })
-  calendar.render()
+  calendar.render();
+
+  calendar.addEvent({
+    title: 'Nuevo evento',
+    start: '2025-07-20',
+    allDay: true // puede ser false si tiene hora
+  });
 })
